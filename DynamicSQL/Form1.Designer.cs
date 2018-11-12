bd44db4390386fcc915c047c9af0a6f05513871b
AHronec7 <austinhronec16@yahoo.com>
2018-11-12 09:48:02 -0500

    Dynamic sql commited

---
+++ DynamicSQL/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace DynamicSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.DBcombobox = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a Database...";
+            // 
+            // DBcombobox
+            // 
+            this.DBcombobox.FormattingEnabled = true;
+            this.DBcombobox.Location = new System.Drawing.Point(23, 41);
+            this.DBcombobox.Name = "DBcombobox";
+            this.DBcombobox.Size = new System.Drawing.Size(137, 21);
+            this.DBcombobox.TabIndex = 1;
+            this.DBcombobox.SelectedIndexChanged += new System.EventHandler(this.DBcombobox_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 94);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(217, 212);
+            this.listBox1.TabIndex = 2;
+          
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(276, 94);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(222, 212);
+            this.listBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 312);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.DBcombobox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox DBcombobox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ DynamicSQL/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace DynamicSQL
+{
+    public partial class Form1 : Form
+    {
+        //establishing a connection
+
+        const string connstring = @"server=DTPLAPTOP11;Database=Adventureworks2012; 
+                                 Trusted_Connection=True";
+        SqlConnection sqlconnection = new SqlConnection(connstring);
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            SqlCommand sqlcmd = new SqlCommand("select * From sys.databases", sqlconnection);
+            SqlDataAdapter da = new SqlDataAdapter(sqlcmd);
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+
+
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                string DBName = dt.Rows[i][0].ToString();
+                DBcombobox.Items.Add(DBName);
+            }
+        }
+
+
+
+        private void DBcombobox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string connectDB = (@"Data Source=DTPLAPTOP11;Initial Catalog=" + DBcombobox.SelectedItem + ";Trusted_Connection=True");
+            SqlConnection sql = new SqlConnection(connectDB);
+
+            SqlCommand sqlcmd = new SqlCommand((@"select Table_Schema, Table_name FROM INFORMATION_SCHEMA.TABLES UNION select Specific_Schema , Specific_Name FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE='PROCEDURE'"), sql);
+            SqlDataAdapter sqlda = new SqlDataAdapter(sqlcmd);
+            DataTable dt = new DataTable();
+
+            sqlda.Fill(dt);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                string db = dt.Rows[i][0].ToString() + "." + dt.Rows[i][1].ToString();
+                listBox1.Items.Add(db);
+
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string connectdb = (@"Data Source=DTPLAPTOP11;Initial Catalog=" + listBox1.SelectedItem + ";Trusted_Connection=True");
+            SqlConnection sqlconnnect = new SqlConnection(connectdb);
+
+            SqlCommand sqlcmd = new SqlCommand((@"EXEC sp_depends @objectname= N '" + listBox1.SelectedItem + " ';"), sqlconnnect);
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter sqlda = new SqlDataAdapter(sqlcmd);
+            //commmand type stored procedure
+            DataSet ds = new DataSet();
+            sqlda.Fill(ds);
+
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                listBox2.Items.Add(ds.Tables[0].Rows[i][0].ToString());
+            }
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                listBox2.Items.Add(ds.Tables[0].Rows[i][0].ToString());
+            }
+
+
+
+        }
+    }
+}
+
+     
+    
+
